de5f8ca33f9a5c4229707901f5d6af16212cb81f
Minter <adf5858@naver.com>
2025-07-07 13:12:25 +0900

    Initial Commit

---
+++ DailyWater/DailyWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWaterApp.swift
+//  DailyWater
+//
+//  Created by 이민지 on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

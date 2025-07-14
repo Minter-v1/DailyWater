9976fa4c6164074663adb69c677f28422e84ef9b
Minter <adf5858@naver.com>
2025-07-14 19:02:41 +0900

    Feat(model): 사용자 설정 관련 모델 구현

--- DailyWater/Core/Models/UserSetting.swift
+++ DailyWater/Core/Models/UserSetting.swift
@@ -10,7 +10,7 @@ import Foundation
 // TODO: - 사용자 설정 데이터 모델 구현
 struct UserSetting: Codable, Equatable {
     // 일일 목표량
-    var dailyGoal: Int
+    var dailyGoal: Double
     
     // 알람 설정
     var notification: Bool
@@ -21,8 +21,81 @@ struct UserSetting: Codable, Equatable {
     // 앱 사용 시작일
     var startDate: Date
     
+    init(
+        dailyGoal: Double = 2000,
+        notification: Bool = true,
+        recommandation: Bool = true,
+        startDate: Date = Date()
+    ) {
+        self.dailyGoal = dailyGoal
+        self.notification = notification
+        self.recommandation = recommandation
+        self.startDate = startDate
+    }
     
 }
 
 
-// TODO: - 계산된 프로퍼티
+
+
+
+// MARK: - 알람 시간 설정
+struct NotificationTime: Identifiable, Codable, Equatable {
+    let id: UUID
+    let hour: Int
+    let minute: Int
+    
+    // 생성자(내부 UUID 할당)
+    init (hour: Int, minute: Int) {
+        self.id = UUID()
+        self.hour = hour
+        self.minute = minute
+    }
+    
+    // 시간 -> Date 객체 변환
+    var dateComponent: DateComponents {
+        var components = DateComponents()
+        components.hour = hour
+        components.minute = minute
+        return components
+    }
+}
+
+
+
+// MARK: - 고정된 알림 시간(09:00~21:00, by: 3h)
+extension NotificationTime {
+    static var fixedTiems: [NotificationTime] {
+        [
+            NotificationTime(hour: 9, minute: 0),
+            NotificationTime(hour: 12, minute: 0),
+            NotificationTime(hour: 15, minute: 0),
+            NotificationTime(hour: 18, minute: 0),
+            NotificationTime(hour: 21, minute: 0),
+            
+        ]
+    }
+}
+
+
+// MARK: - 샘플 데이터
+extension UserSetting {
+    static var `default`: UserSetting {
+        UserSetting()
+    }
+    
+    // 많은 물의 양
+    static var highGoal: UserSetting {
+        UserSetting (
+            dailyGoal: 3000
+        )
+    }
+    
+    // 알림 비활성화
+    static var noNotification: UserSetting {
+        UserSetting(
+            dailyGoal: 1500,
+            notification: false
+        )
+    }
+}
